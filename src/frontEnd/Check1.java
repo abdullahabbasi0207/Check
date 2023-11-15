2986c8e1334b489ee357a074737ddd6de186b45a
abdullah <nerocrest2070@gmail.com>
2023-11-15 19:45:40 +0530

    conflict2

--- src/frontEnd/Check1.java
+++ src/frontEnd/Check1.java
@@ -7,5 +7,12 @@ public class Check1 {
 		System.out.println("Check3");
 		System.out.println("Check2");
 		
+<<<<<<< Updated upstream
+=======
+		
+		System.out.println("uncommit");
+	
+		String name  = " Abdullah ";
+>>>>>>> Stashed changes
 	}
 }
